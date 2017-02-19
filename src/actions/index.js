9bbc5925cf643e65fdc1849358aab1e659266b93
Greg <gperlman27@gmail.com>
2017-02-18 19:06:16 -0500

    add types and auth reducers

---
+++ src/actions/index.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from './types';
+
+const ROOT_URL = 'http://localhost:8000';
+
+
+// redux thunk allows us to return a FUNCTION rather than an OBJECT
+
+export function signinUser({email, password}){
+  // submit email/password to the server
+  //
+  // if request is good...
+  // update starte to indicate user is authenticated
+  // saved JWT token
+  // redirect to the route '/feature'
+  //
+  // if request is bad...
+  // show an error to the user
+  return function(dispatch){
+    axios.post(`${ROOT_URL}/api/signin`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/dashboard');
+      })
+      .catch(() => {
+        dispatch(authError('Bad Login Info'));
+      });
+  }
+}
+
+export function authError(error){
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  }
+}
+
+export function signoutUser(){
+  localStorage.removeItem('token');
+
+  return { type: UNAUTH_USER }
+}
